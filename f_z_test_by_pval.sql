2f9978eb2f5c9d1a89022043486ff672720274bb
ccrosbie <crosbie.christopher.p@gmail.com>
2015-06-05 17:16:56 -0700

    Update f_z_test_by_pval.sql

--- f_z_test_by_pval.sql
+++ f_z_test_by_pval.sql
@@ -1,5 +1,6 @@
 create function f_z_test_by_pval(alpha float, x_bar float, test_val float, sigma float, n float)
---An independent one-sample z-test is used to test whether the average of a sample differ significantly from a population mean. 
+--An independent one-sample z-test is used to test whether the average 
+--of a sample differ significantly from a population mean. 
 --INPUTS:
 -- alpha = sigificant level which we will accept or reject at
 -- x_bar = sample mean
