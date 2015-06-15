1888bc4f9b3217944fdc88cf0907804143a5604b
ccrosbie <crosbie.christopher.p@gmail.com>
2015-06-15 12:35:37 -0700

    Create Create_IQR_Aggregate.sql

---
+++ Create_IQR_Aggregate.sql
@@ -0,0 +1,37 @@
+CREATE FUNCTION f_init_iqr ()
+  RETURNS varchar
+IMMUTABLE
+AS $$
+  return ''
+$$ LANGUAGE plpythonu;
+
+
+CREATE FUNCTION f_agg_iqr (state varchar, a float)
+  RETURNS varchar
+STABLE
+AS $$
+  if state:
+    return state + ', ' + str(a)
+  return str(a)
+$$ LANGUAGE plpythonu;
+
+
+CREATE FUNCTION f_fin_iqr (a varchar)
+  RETURNS float
+STABLE 
+AS $$ 
+	import numpy as numpy
+	if a:
+   		arr = numpy.array(a.split(","),numpy.float)  
+   		x = numpy.percentile(arr, 75) #- numpy.percentile(arr, 25)
+	else:
+   		x = 0
+$$ LANGUAGE plpythonu;
+
+
+CREATE AGGREGATE f_iqr (float) 
+(
+initfunc = f_init_iqr,
+aggfunc = f_agg_iqr,
+finalizefunc = f_fin_iqr
+);
